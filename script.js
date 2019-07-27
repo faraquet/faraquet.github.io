dad1a39cb964e187d80ca9d0e967e7598cb790b1
Evgenia Petukhova <47631546+eugenia1190@users.noreply.github.com>
2019-07-27 10:44:16 +0500

    add anchors (#4)
    * add anchors
    
    * move articles to rails
    
    * change style for reading list

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ fetch(url)
 	.then (json => {
 		avatar.setAttribute('src', json.avatar_url);
 	});
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href');
+    document.querySelector('' + blockID).scrollIntoView({
+    	behavior: 'smooth',
+    	block: 'start'
+    })
+  });
+}
